415390f62fb0e3783a83bf6e9b08cc05ada16871
Eric Weitzman <Eweitzman@edu.uwaterloo.ca>
2020-08-28 22:42:39 -0400

    added /test

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import sqlalchemy
 from flask_marshmallow import Marshmallow
+import json
 
 
 
@@ -8,4 +9,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world! Eric!'
+    jsonString = """ "firstName": "Eric", 
+                      "lastName": "Weitzman" """
+    jsonVar1 = jsonify("hello", "world")
+    jsonVar2 = jsonify(jsonString)
+    return jsonString
+
+
+@app.route('/test', methods=['get', 'post'])
+def test():
+    jsonVar3  = 'hello world'
+
+    jsonVar4 = '''
+    {
+        "people": [
+            {
+                "name": "John Smith",
+                "phone": "615-555-7164",
+                "emails": ["johnsmith@bogusemail.com", "john.smith@work-place.com"],
+                "has_license": false
+            },
+            {
+                "name": "Jane Doe",
+                "phone": "560-555-5153",
+                "emails": null,
+                "has_license": true
+            }
+        ]
+    }
+    '''
+    #  loads: string -> json
+    data = json.loads(jsonVar4)
+    # to go back to string, dump : json -> string
+    jsonData = json.dumps(data)
+
+    # print(data)
+
+    return data
